added8b5f99231d05f5e5caf2574e8ee06dee88b
lsmit188 <112022082+lsmit188@users.noreply.github.com>
2024-12-02 14:50:46 -0700

    Part 2

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-const data = {
-    title: "Welcome to CS3210 Template Engine Lab",
-    content: "This is a simple template engine demonstration.",
-    author: "Dr.Red",
-};
-
-const template = `
-    <h3>{{title}}</h3>
-    <p>{{content}}</p>
-    <p>Author: {{author}}</p>
+// Define a template using Mustache.js
+var template = `
+    <ul>
+        {{#data}}
+        <li>Name: {{name}}, Age: {{age}}, City: {{city}}</li>
+        {{/data}}
+    </ul>
 `;
 
-function renderTemplate(template, data) {
-    for (const key in data) {
-        const regex = new RegExp('{{' + key + '}}', 'g');
-        template = template.replace(regex, data[key]);
-    }
-    return template;
-}
+// Get the 'output' div element
+var output = document.getElementById('output');
 
-const app = document.getElementById('app');
-app.innerHTML = renderTemplate(template, data);
+// Fetch JSON data from the file
+fetch('data.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        // Render the data using the template
+        var rendered = Mustache.render(template, { data: data });
+        output.innerHTML = rendered;
+    })
+    .catch(function(error) {
+        console.error('Error:', error);
+    });
